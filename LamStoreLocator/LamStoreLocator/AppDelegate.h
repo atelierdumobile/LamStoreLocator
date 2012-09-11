59605e21bef58806638419b9dbe1dd31e6303c56
Benjamin PAYEN <contact@benjaminpayen.fr>
2012-09-11 12:05:49 +0200

    Create Xcode project

---
+++ LamStoreLocator/LamStoreLocator/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  LamStoreLocator
+//
+//  Created by benjamin payen on 11/09/12.
+//  Copyright (c) 2012 L'atelier du mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ LamStoreLocator/LamStoreLocator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LamStoreLocator
+//
+//  Created by benjamin payen on 11/09/12.
+//  Copyright (c) 2012 L'atelier du mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
